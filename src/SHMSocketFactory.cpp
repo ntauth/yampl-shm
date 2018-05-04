f6a67dfdcc79107db43614884efb35260d03e6f1
Ayoub Chouak <a.chouak@protonmail.com>
2018-05-04 20:54:38 +0200

    Fixed wrong SCY_PLUGIN declaration

--- src/SHMSocketFactory.cpp
+++ src/SHMSocketFactory.cpp
@@ -8,7 +8,7 @@ namespace yampl
 {
     namespace shm
     {
-        SCY_PLUGIN(SHMSocketFactory, "yampl-shm", SHMSocketFactory::PLUGIN_VERSION)
+        SCY_PLUGIN(SocketFactory, "yampl-shm", SocketFactory::PLUGIN_VERSION)
 
         ISocket *SocketFactory::createClientSocket(Channel channel, Semantics semantics, void (*deallocator)(void *, void *), const std::string& name){
             return new ClientSocket(channel, semantics, deallocator, name);
