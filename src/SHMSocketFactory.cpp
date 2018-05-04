2cd34252e01775f87ce5c655531de317db914120
Ayoub Chouak <a.chouak@protonmail.com>
2018-05-04 20:50:24 +0200

    Fixed SCY_PLUGIN missing declaration for SHMSocketFactory

--- src/SHMSocketFactory.cpp
+++ src/SHMSocketFactory.cpp
@@ -4,16 +4,18 @@
 #include "ClientSocket.h"
 #include "ServerSocket.h"
 
-namespace yampl{
-namespace shm{
+namespace yampl
+{
+    namespace shm
+    {
+        SCY_PLUGIN(SHMSocketFactory, "yampl-shm", SHMSocketFactory::PLUGIN_VERSION)
 
-  ISocket *SocketFactory::createClientSocket(Channel channel, Semantics semantics, void (*deallocator)(void *, void *), const std::string& name){
-  return new ClientSocket(channel, semantics, deallocator, name);
-}
+        ISocket *SocketFactory::createClientSocket(Channel channel, Semantics semantics, void (*deallocator)(void *, void *), const std::string& name){
+            return new ClientSocket(channel, semantics, deallocator, name);
+        }
 
-ISocket *SocketFactory::createServerSocket(Channel channel, Semantics semantics, void (*deallocator)(void *, void *)){
-  return new ServerSocket(channel, semantics, deallocator);
-}
-
-}
+        ISocket *SocketFactory::createServerSocket(Channel channel, Semantics semantics, void (*deallocator)(void *, void *)){
+            return new ServerSocket(channel, semantics, deallocator);
+        }
+    }
 }
